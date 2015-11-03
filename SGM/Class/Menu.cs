4468cb88002f6eb6752f90b23d0a4ddf6bee2f6f
Jannick Fahlbusch <git@jf-projects.de>
2015-11-03 15:58:46 +0100

    Added the raw class-structure

---
+++ SGM/Class/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SGM
+{
+	public class Menu
+	{
+		public Menu ()
+		{
+		}
+	}
+}
+
---
+++ SGM/Class/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SGM
+{
+	public class Order
+	{
+		public Order ()
+		{
+		}
+	}
+}
+
