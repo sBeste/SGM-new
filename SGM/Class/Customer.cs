0937316dff35af7134c26477a46c8e0455adb400
Jannick Fahlbusch <git@jf-projects.de>
2015-11-03 16:17:43 +0100

    Customer and Customer.pay()

--- SGM/Class/Customer.cs
+++ SGM/Class/Customer.cs
@@ -4,9 +4,21 @@ namespace SGM
 {
 	public class Customer
 	{
-		public Customer ()
+		public string Name {get; private set;}
+		public string FirstName {get; private set;}
+		public double CreditBalance {get; private set;}
+
+		public Customer (string FirstName, string Name, double CreditBalance)
 		{
+			this.FirstName = FirstName;
+			this.Name = Name;
+			this.CreditBalance = CreditBalance;
+		}
+
+		public double pay(double amount) {
+			this.CreditBalance = this.CreditBalance - amount;
+
+			return this.CreditBalance;
 		}
 	}
 }
-
