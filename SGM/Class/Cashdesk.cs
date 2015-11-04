4b7e981f7c5eb303cf8bf542a318193296d121a1
Jannick Fahlbusch <git@jf-projects.de>
2015-11-04 18:32:43 +0100

    First version of the caschdesk

--- SGM/Class/Cashdesk.cs
+++ SGM/Class/Cashdesk.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SGM
 {
 	public class Cashdesk
 	{
+		public Queue<Customer> Queue = new Queue<Customer>();
+		public Customer CurrentCustomer;
+		public double Cash {get; private set;}
+
 		public Cashdesk ()
 		{
 		}
+
+		public int customerAmount()
+		{
+			return Queue.Count;
+		}
+
+		public Customer nextCustomer()
+		{
+			CurrentCustomer = Queue.Dequeue();
+			return CurrentCustomer;
+		}
+
+		public void chargeCustomer(double amount)
+		{
+			CurrentCustomer.pay(amount);
+		}
 	}
 }
-
