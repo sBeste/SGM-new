9f6ef710ace2cc22ec45e1dde663d29085e4481d
Jannick Fahlbusch <git@jf-projects.de>
2015-11-04 17:59:22 +0100

    Basic structure of the Menu

--- SGM/Class/Menu.cs
+++ SGM/Class/Menu.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SGM
 {
 	public class Menu
 	{
-		public Menu ()
+		public string Name {get; private set;}
+		public double Price {get; private set;}
+		public int Stock {get; private set;}
+		public List<Product> ProductList = new List<Product>();
+
+		public Menu (string Name, double Price, List<Product> ProductList, int Stock)
 		{
+			this.Name = Name;
+			this.Price = Price;
+			this.Stock = Stock;
+			this.ProductList.AddRange(ProductList);
+		}
+
+		public int setStock (int Stock)
+		{
+				this.Stock = Stock;
+
+				return this.Stock;
+		}
+
+		public int decreaseStock (int DecreaseAmount)
+		{
+			this.Stock = this.Stock - DecreaseAmount;
+
+			return this.Stock;
 		}
 	}
 }
-
